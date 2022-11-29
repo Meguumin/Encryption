7fc212e901535743863e5d4cf4f8d325a5016221
Aiden Tong <93221974+Meguumin@users.noreply.github.com>
2022-11-29 19:01:40 +0000

    Big changes

--- .ccls-cache/@home@runner@Encryption/.ccls-cache@@home@runner@Encryption@main.cpp
+++ .ccls-cache/@home@runner@Encryption/.ccls-cache@@home@runner@Encryption@main.cpp
@@ -4,14 +4,7 @@
 #include <iostream>
 #include <string.h>
 #include <string>
-#include <cstdlib>
-#include <ctime>
-#include <stdio.h>
-void clear() {
-    // CSI[2J clears screen, CSI[H moves the cursor to top-left corner
-    std::cout << "\x1B[2J\x1B[H";
-}
-//Encrypt works but decrypt doesnt
+
 void XorEN(std::ofstream &MyFile, char *key, std::string x) {
       int t = strlen(key);
       int b = 0;
@@ -33,11 +26,12 @@ void encrypt(char *xorKey, std::string x) {
   std::ifstream read("target.txt");
   MyFile.open("Encrypted.txt");
   const char *c = x.c_str();
-  x.assign( (std::istreambuf_iterator<char>(read) ),
-  (std::istreambuf_iterator<char>()    ) );
-    // std::cout << x << "\n";
-       XorEN(MyFile, xorKey, x);
+  while(getline(read, x))
+    {
+      print("%s", x)
+    }
   // std::puts(c);
+  XorEN(MyFile, xorKey, x);
   // std:: cout << std::endl;
     printf("Encrypted!");
   MyFile.close();
@@ -45,47 +39,21 @@ void encrypt(char *xorKey, std::string x) {
 
 void decrypt(char *key, std::string x) {
   std::ifstream geten("Encrypted.txt");
-   x.assign( (std::istreambuf_iterator<char>(geten) ),
-  (std::istreambuf_iterator<char>()    ) );
-   //std::cout << x << "\n";
+  getline(geten, x);
   std::ofstream MyFile;
   MyFile.open("Decrypted.txt");
   XorEN(MyFile, key, x);
   printf("Decrypted!");
   MyFile.close();
 }
-
 int main() {
-  char xorKey[] = "Secure Key";
+  char xorKey[] = "Hello";
   std::string Input;
   std::string Output;
-  std::string keypath;
-  std::string tmp;
-  std::string targetfile;
   int userinput;
-  printf("Type in Key(1) or Load from Text File?(2)\n");
-  scanf("%d", &userinput);
-  clear();
-  if(userinput == 1)
-  {
-     printf("Enter Key:\n");
-     fgets(xorKey, 1000, stdin);
-     clear();
-  }
-  else if(userinput == 2)
-  {
-    printf("Enter Keypath:\n");
-    std::cin >> keypath;
-    std::ifstream getkey(keypath); 
-    tmp.assign( (std::istreambuf_iterator<char>(getkey) ),
-  (std::istreambuf_iterator<char>()    ) );
-    strcpy(xorKey, tmp.c_str());
-    clear();
-  }
-  //printf("%s\n", xorKey);
+
   printf("Encrypt(1) or Decrypt(2)?\n");
   scanf("%d", &userinput);
-  clear();
   switch(userinput) {
   case 1:
     encrypt(xorKey, Input);
--- .ccls-cache/@home@runner@Encryption/main.cpp
+++ .ccls-cache/@home@runner@Encryption/main.cpp
@@ -4,7 +4,14 @@
 #include <iostream>
 #include <string.h>
 #include <string>
-
+#include <cstdlib>
+#include <ctime>
+#include <stdio.h>
+void clear() {
+    // CSI[2J clears screen, CSI[H moves the cursor to top-left corner
+    std::cout << "\x1B[2J\x1B[H";
+}
+//Encrypt works but decrypt doesnt
 void XorEN(std::ofstream &MyFile, char *key, std::string x) {
       int t = strlen(key);
       int b = 0;
@@ -26,9 +33,11 @@ void encrypt(char *xorKey, std::string x) {
   std::ifstream read("target.txt");
   MyFile.open("Encrypted.txt");
   const char *c = x.c_str();
-  getline(read, x);
+  x.assign( (std::istreambuf_iterator<char>(read) ),
+  (std::istreambuf_iterator<char>()    ) );
+    // std::cout << x << "\n";
+       XorEN(MyFile, xorKey, x);
   // std::puts(c);
-  XorEN(MyFile, xorKey, x);
   // std:: cout << std::endl;
     printf("Encrypted!");
   MyFile.close();
@@ -36,21 +45,47 @@ void encrypt(char *xorKey, std::string x) {
 
 void decrypt(char *key, std::string x) {
   std::ifstream geten("Encrypted.txt");
-  getline(geten, x);
+   x.assign( (std::istreambuf_iterator<char>(geten) ),
+  (std::istreambuf_iterator<char>()    ) );
+   //std::cout << x << "\n";
   std::ofstream MyFile;
   MyFile.open("Decrypted.txt");
   XorEN(MyFile, key, x);
   printf("Decrypted!");
   MyFile.close();
 }
+
 int main() {
-  char xorKey[] = "Ching chong chinaman";
+  char xorKey[] = "Secure Key";
   std::string Input;
   std::string Output;
+  std::string keypath;
+  std::string tmp;
+  std::string targetfile;
   int userinput;
-
+  printf("Type in Key(1) or Load from Text File?(2)\n");
+  scanf("%d", &userinput);
+  clear();
+  if(userinput == 1)
+  {
+     printf("Enter Key:\n");
+     fgets(xorKey, 1000, stdin);
+     clear();
+  }
+  else if(userinput == 2)
+  {
+    printf("Enter Keypath:\n");
+    std::cin >> keypath;
+    std::ifstream getkey(keypath); 
+    tmp.assign( (std::istreambuf_iterator<char>(getkey) ),
+  (std::istreambuf_iterator<char>()    ) );
+    strcpy(xorKey, tmp.c_str());
+    clear();
+  }
+  //printf("%s\n", xorKey);
   printf("Encrypt(1) or Decrypt(2)?\n");
   scanf("%d", &userinput);
+  clear();
   switch(userinput) {
   case 1:
     encrypt(xorKey, Input);
